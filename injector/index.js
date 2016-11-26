e97b9e019eafe5e7ca6b7f0f612c4bcb36d91828
xrocketmanx <xrocketmanx007@gmail.com>
2016-11-26 18:05:37 +0200

    create dependency injector

---
+++ injector/index.js
@@ -0,0 +1,55 @@
+var path = require('path');
+
+function Injector(dir, config) {
+    var modules = {};
+
+    this.get = function(moduleName) {
+        return modules[moduleName];
+    };
+
+    this.extendLibrary = function(modules) {
+        for (var i in modules) {
+            modules[i] = modules[i];
+        }
+    };
+
+    this.load = function() {
+        for (var name in config) {
+            resolveModule(name);
+        }
+        return this;
+    };
+
+    function resolveModule(name) {
+        if (!modules[name]) {
+            var modulePath = path.join(dir, config[name]);
+            var module = require(modulePath);
+
+            if (module.inject) {
+                var required = getArguments(module.toString());
+                required = required.map(function(arg) {
+                    if (!modules[arg]) {
+                        resolveModule(arg);
+                    }
+                    return modules[arg];
+                });
+                modules[name] = module.apply(null, required);
+            } else {
+                modules[name] = module;
+            }
+        }
+    }
+
+    var argsPattern = /\((.*)\)/;
+    function getArguments(module) {
+        return module.match(argsPattern)[1].split(/,\s*/);
+    }
+}
+
+module.exports = Injector;
+
+/*
+Config: {
+    Name: path
+}
+ */
